824daf02aa2cfae37b5ac5dd53c0ac29426a02ef
Peter Kosztolanyi <peter.kosztolanyi@gmail.com>
2017-01-03 15:08:21 +0100

    Added context variables to jenkinsfile and run project jobs recursively

--- test.sh
+++ test.sh
@@ -2,10 +2,12 @@
 KINESIS_PATH=~/bin/
 RET=0
 
-for project in `find . -name kinesis.json -exec dirname {} \;`
+for project in `find . -name project.json -exec dirname {} \;`
 do
+CONTEXT=$project/context/dev1.json
+
     echo "Testing $project"
-    java -jar $KINESIS_PATH/kinesis-2.0.0-SNAPSHOT-standalone.jar -p $project
+    java -jar $KINESIS_PATH/kinesis-2.0.0-SNAPSHOT-standalone.jar -p $project -c $CONTEXT -r
     if [ $? != 0 ]
     then
         RET=1
