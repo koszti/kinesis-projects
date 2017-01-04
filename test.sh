f9b747a16fa916f4f41075669ee8f04425c4505d
Peter Kosztolanyi <peter.kosztolanyi@gmail.com>
2017-01-04 11:35:38 +0100

    Fixed test runner schell script - Run jobs recursively with context variables

--- test.sh
+++ test.sh
@@ -6,18 +6,13 @@ for project in `find . -name project.json -exec dirname {} \;`
 do
 CONTEXT=$project/context/dev1.json
 
-    echo "Testing $project"
-    for test in `find $project -name kinesis.json -exec dirname {} \; | sort`
-    do
-
-        echo "Running test $test"
-        java -jar $KINESIS_PATH/kinesis-2.0.0-SNAPSHOT-standalone.jar -p $project -c $CONTEXT
-        if [ $? != 0 ]
-        then
-            RET=1
-	    exit $RET
-        fi
-    done
+    echo "Running tests from $project project"
+    java -jar $KINESIS_PATH/kinesis-2.0.0-SNAPSHOT-standalone.jar -p $project -c $CONTEXT -r -v
+    if [ $? != 0 ]
+    then
+        RET=1
+        exit $RET
+    fi
 done
 
 exit $RET
