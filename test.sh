41fe977832d1c8c29bcffeb6c1cc231bbf624427
Peter Kosztolanyi <peter.kosztolanyi@gmail.com>
2017-02-15 21:02:13 +0000

    testing jenkins build..

--- test.sh
+++ test.sh
@@ -4,8 +4,6 @@ CONTEXT=dev1.json
 
 RET=0
 
-echo "Kinesis CLI executable: [$KINESIS]"
-
 # Avoid issues with directories having spaces
 SAVEIFS=$IFS
 IFS=$(echo -en "\n")
@@ -13,11 +11,15 @@ IFS=$(echo -en "\n")
 # Find every Kinesis project and run in a loop
 for project in `find "$(pwd)" -name project.json`
 do
-echo "Kinesis CLI executable: [$KINESIS]"
+    echo "Kinesis CLI executable: [$KINESIS]"
 
     # Make absolute paths that is required for Kinesis CLI
     PROJECT_DIR="$(dirname $project)"
     CONTEXT_PATH="$PROJECT_DIR/context/$CONTEXT"
+    
+    echo "Kinesis: [$KINESIS]"
+    echo "Project Dir: [$PROJECT_DIR]"
+    echo "Context Path: [$CONTEXT_PATH]"
 
     echo "Running tests from $PROJECT_DIR project"
     $KINESIS -p $PROJECT_DIR -c $CONTEXT_PATH -t $CANVAS_TIMEOUT -r
