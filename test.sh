60646a0017d949ec0106123464fe5fe5914ef722
Peter Kosztolanyi <peter.kosztolanyi@gmail.com>
2017-02-15 21:51:02 +0000

    testing jenkins build..

--- test.sh
+++ test.sh
@@ -12,7 +12,6 @@ PROJECT_DIR=$1
 CONTEXT_PATH="$PROJECT_DIR"/context/$CONTEXT
 
     echo "Running tests in $PROJECT_DIR project"
-    echo "$KINESIS" -p "$PROJECT_DIR" -c "$CONTEXT_PATH" -t "$CANVAS_TIMEOUT" -r
     "$KINESIS" -p "$PROJECT_DIR" -c "$CONTEXT_PATH" -t "$CANVAS_TIMEOUT" -r
     if [ $? != 0 ]
     then
